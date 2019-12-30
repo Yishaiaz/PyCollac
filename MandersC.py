6b55388744458860abd7cce0f8108402233ff8f9
yishaia zabary <yz@sky-pick.com>
2019-12-30 14:19:42 +0200

    Pearson and Manders multi file processing

--- MandersC.py
+++ MandersC.py
@@ -1,6 +1,9 @@
 import numpy as np
 import pandas as pd
 from vectorPreProcess import vectorsPreProcces as vpp
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
 
 class MandersC:
@@ -90,4 +93,47 @@ class MandersC:
                 np.sum(np.power(v2.values, 2, dtype="float")))
             return total_nom / total_denom
 
+sns.set()
 
+mandersMocForEachZForEachFile = pd.DataFrame(columns=['file_name', 'Z=1', 'Z=2', 'Z=3', 'Z=4', 'Z=5'])
+MAIN_DIR_NAME = "/Users/yishaiazabary/Desktop/University/DNA Deformat proteins research/extractedStatistics/"
+# MAIN_DIR_NAME = "/Users/yishaiazabary/PycharmProjects/PyCollac/Extracted spots detection by FIJI/A5_M_H3K27ac_488_Rab_H3K4me1_546_001.nd2 - A5_M_H3K27ac_488_Rab_H3K4me1_546_001.nd2 (series 1)"
+path_to_all_channels_dirs = MAIN_DIR_NAME
+channels_dirs = [dir_name[0]+"/" for dir_name in os.walk(path_to_all_channels_dirs)][1:]
+channel1FilesList = [file_path[2] for file_path in os.walk(channels_dirs[0])][0]
+channel1FilesList.remove('.DS_Store')
+channel2FilesLisr = [file_path[2] for file_path in os.walk(channels_dirs[1])][0]
+channel2FilesLisr.remove('.DS_Store')
+for file_ctr in range(len(channel1FilesList)):
+    # Green to Red
+    first_channel_objects = pd.read_csv(channels_dirs[0]+channel1FilesList[file_ctr]).loc[:, ['X', 'Y', 'Z', 'IntDen']]
+    z_levels = first_channel_objects['Z'].astype('int').unique()
+    second_channel_objects = pd.read_csv(channels_dirs[1]+channel2FilesLisr[file_ctr]).loc[:, ['X', 'Y', 'Z', 'IntDen']]
+    # Red to Green
+    # first_channel_objects = pd.read_csv(channels_dirs[1] + channel2FilesLisr[file_ctr]).loc[:,
+    #                         ['X', 'Y', 'Z', 'IntDen']]
+    # z_levels = first_channel_objects['Z'].astype('int').unique()
+    # second_channel_objects = pd.read_csv(channels_dirs[0] + channel1FilesList[file_ctr]).loc[:,
+    #                          ['X', 'Y', 'Z', 'IntDen']]
+
+    mc = MandersC(is_objects=True, z_levels=np.sort(z_levels), intensity_feature_name="IntDen")
+    print("analyzed: "+channel1FilesList[file_ctr])
+    res = mc.calc_MOC_for_single_file(first_channel_objects, second_channel_objects)
+    # res = mc.calc_m_for_single_file(first_channel_objects, second_channel_objects, True)
+    while len(res) < 5:
+        res += [0]
+    file_title = channel1FilesList[file_ctr][:-8].split('_')
+    file_title = file_title[0].split(' ')[4] + "_"+file_title[2] + "_" + file_title[3]
+    mandersMocForEachZForEachFile.loc[file_ctr] = [file_title] + res
+
+mandersMocForEachZForEachFile = mandersMocForEachZForEachFile.set_index('file_name', drop=True)
+mandersMocForEachZForEachFile = mandersMocForEachZForEachFile.astype(float)
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(mandersMocForEachZForEachFile, annot=True, ax=ax)
+ax.set_title = "Manders RedToGreen"
+
+plt.tight_layout()
+plt.show()
+dir = "InitialResults/MandersCoeff/MOC_CALCS/"
+plot_file_name = "GreenToRedMoc"
+f.savefig("{0}{1}.png".format(dir, plot_file_name), dpi=200)
--- PearsonsC.py
+++ PearsonsC.py
@@ -1,6 +1,10 @@
+import os
+
 import numpy as np
 import pandas as pd
 from vectorPreProcess import vectorsPreProcces as vpp
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 class PearsonC:
@@ -20,7 +24,13 @@ class PearsonC:
                 v1_trimmed = v1.loc[(v1['Z'] >= self.z_levels[i]) * (v1['Z'] < self.z_levels[i + 1])]
                 v2_trimmed = v2.loc[(v2['Z'] >= self.z_levels[i]) * (v2['Z'] < self.z_levels[i + 1])]
                 v1_trimmed, v2_trimmed = self.vpp.same_length_vectors(v1_trimmed, v2_trimmed)
-                coeff_for_z_level.append(self.calc_pearson_objects(v1_trimmed, v2_trimmed))
+                curr_coeff = 0 if np.sum(v1_trimmed.values)==0 or np.sum(v2_trimmed.values)==0 else self.calc_pearson_objects(v1_trimmed, v2_trimmed)
+                coeff_for_z_level.append(curr_coeff)
+
+            last_z_v1_trimmed, last_z_v2_trimmed = self.vpp.same_length_vectors(v1.loc[(v1['Z'] >= self.z_levels[len(z_levels)-1])],
+                                                                                v2.loc[(v2['Z'] >= self.z_levels[len(z_levels)-1])])
+            curr_coeff = 0 if np.sum(last_z_v1_trimmed.values) == 0 or np.sum(last_z_v2_trimmed.values) == 0 else self.calc_pearson_objects(last_z_v1_trimmed, last_z_v2_trimmed)
+            coeff_for_z_level.append(curr_coeff)
             return coeff_for_z_level
         elif self.is_objects:
             return self.calc_pearson_objects(v1, v2)
@@ -56,3 +66,45 @@ class PearsonC:
                                dtype="float")) * np.sqrt(
             np.sum(np.power(im2_pixels_intensities_average_subtracted, 2, dtype="float"), dtype="float"))
         return nom / denom
+
+sns.set()
+
+pearsonForEachZForEachFile = pd.DataFrame(columns=['file_name', 'Z=1', 'Z=2', 'Z=3', 'Z=4', 'Z=5'])
+MAIN_DIR_NAME = "/Users/yishaiazabary/Desktop/University/DNA Deformat proteins research/extractedStatistics/"
+# MAIN_DIR_NAME = "/Users/yishaiazabary/PycharmProjects/PyCollac/Extracted spots detection by FIJI/A5_M_H3K27ac_488_Rab_H3K4me1_546_001.nd2 - A5_M_H3K27ac_488_Rab_H3K4me1_546_001.nd2 (series 1)"
+path_to_all_channels_dirs = MAIN_DIR_NAME
+channels_dirs = [dir_name[0]+"/" for dir_name in os.walk(path_to_all_channels_dirs)][1:]
+channel1FilesList = [file_path[2] for file_path in os.walk(channels_dirs[0])][0]
+channel1FilesList.remove('.DS_Store')
+channel2FilesLisr = [file_path[2] for file_path in os.walk(channels_dirs[1])][0]
+channel2FilesLisr.remove('.DS_Store')
+for file_ctr in range(len(channel1FilesList)):
+    # Green to red
+    first_channel_objects = pd.read_csv(channels_dirs[0]+channel1FilesList[file_ctr]).loc[:, ['X', 'Y', 'Z', 'IntDen']]
+    z_levels = first_channel_objects['Z'].astype('int').unique()
+    second_channel_objects = pd.read_csv(channels_dirs[1]+channel2FilesLisr[file_ctr]).loc[:, ['X', 'Y', 'Z', 'IntDen']]
+    # Red to Green
+    # first_channel_objects = pd.read_csv(channels_dirs[0] + channel2FilesLisr[file_ctr]).loc[:,
+    #                         ['X', 'Y', 'Z', 'IntDen']]
+    # z_levels = first_channel_objects['Z'].astype('int').unique()
+    # second_channel_objects = pd.read_csv(channels_dirs[1] + channel1FilesList[file_ctr]).loc[:,
+    #                          ['X', 'Y', 'Z', 'IntDen']]
+
+    pc = PearsonC(is_objects=True, z_levels=np.sort(z_levels), intensity_feature_name="IntDen")
+    print("analyzed: "+channel1FilesList[file_ctr])
+    res = pc.calc_pearson(first_channel_objects, second_channel_objects, True)
+    while len(res) < 5:
+        res += [0]
+    file_title = channel1FilesList[file_ctr][:-8].split('_')
+    file_title = file_title[0].split(' ')[4] + "_"+file_title[2] + "_" + file_title[3]
+    pearsonForEachZForEachFile.loc[file_ctr] = [file_title]+res
+
+pearsonForEachZForEachFile = pearsonForEachZForEachFile.set_index('file_name', drop=True)
+pearsonForEachZForEachFile = pearsonForEachZForEachFile.astype(float)
+f, ax = plt.subplots(figsize=(9, 6))
+sns.heatmap(pearsonForEachZForEachFile, annot=True, ax=ax)
+ax.set_title = "Pearson RedToGreen"
+
+plt.tight_layout()
+plt.show()
+# f.savefig("RedToGreen.png", dpi=200)
